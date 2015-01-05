b6505c0fc438ee403ab9a80ed4524e664d950fcc
Stanley <stanleyjsdz@gmail.com>
2015-01-05 11:43:17 -0700

    working false evaluator

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,10 +1,83 @@
+require 'pry'
+# require_relative 'mastermind_str_outputs.rb'
+# require_relative 'player_commands.rb'
+
 class Mastermind
+
+  attr_accessor :mastercode, :false_eval
+
+  def initialize
+    @mastercode = 'rrgb'
+  end
+
+
+  def generate
+    @mastercode = []
+    palate = ['r','g','b','y']
+
+    4.times do
+      @mastercode << palate.sample
+    end
+
+    @mastercode = @mastercode.join
+
+  end
+
+  def false_eval(input)
+    @ce_num = 0
+    @cp_num = 0
+    @guess_ary = input.split('')
+    @mcode_ary = @mastercode.split('')
+
+    element_counter(input)
+    position_counter(input)
+  end
+
+  def position_counter(input)
+
+    comparison_ary = @guess_ary.zip(@mcode_ary)
+    comparison_ary.each do |x, y|
+      if x == y
+        @cp_num += 1
+      end
+    end
+
+  end
+
+  def element_counter(input)
+    guess_ary2 = @guess_ary.dup
+    
+    guess_ary2.each do
+      @mcode_ary.each do |m|
+        if guess_ary2[0] == m
+          @ce_num += 1
+        end
+      guess_ary2.shift
+      end
+    end
+
+
+  end
+
+
+
+
+
   def execute(input)
-    secret = "BBGB"
-    if input == secret
+
+    if input == @mastercode
       "You win!"
     else
-      "Guess again!"
+    false_eval(input)
+     "'#{input.upcase}' has #{@ce_num} correct elements with #{@cp_num} in the correct position, guess again!"
+
     end
+
   end
 end
+
+
+# @mm = Mastermind.new
+# @mastercode = @mm.generate
+#
+# puts @mastercode.upcase!
--- test/mastermind_test.rb
+++ test/mastermind_test.rb
@@ -1,15 +1,68 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/mastermind'
+require '/Users/Goldwater/Desktop/Development/mastermind/lib/mastermind.rb'
 
 class MastermindTest < Minitest::Test
+  attr_reader :mm
+
   def test_it_exists
     assert MastermindTest
   end
 
+  def setup
+    @mm = Mastermind.new
+  end
+
+  def test_it_generates_sequence
+    mastercode = @mm.generate
+    assert mastercode
+  end
+
+  def test_sequence_contains_string_of_4
+    @mastercode = @mm.generate
+    seq_length = @mastercode.length
+    assert_equal 4, seq_length
+  end
+
+
+  def test_it_executes_false_input
+    result = @mm.execute('nope')
+    assert result.downcase.include?('again')
+  end
+
+  def test_false_execution_knows_0_correct_elements
+    result = @mm.execute('nope')
+    assert result.downcase.include?('0 correct')
+  end
+
+  def test_false_execution_knows_1_correct_elements
+    result = @mm.execute('rope')
+    assert result.downcase.include?('1 correct')
+    result = @mm.execute('rgrr')
+    assert result.downcase.include?('1 correct')
+  end
+
+  def test_false_execution_knows_3_correct_elements
+    result = @mm.execute('rrge')
+    assert result.downcase.include?('3 correct')
+  end
+
+  def test_it_knows_0_correct_positions
+    result = @mm.execute('nope')
+    assert result.downcase.include?('0 in the correct position')
+  end
+
+  def test_it_knows_1_correct_position
+    result = @mm.execute('rope')
+    assert result.downcase.include?('1 in the correct position')
+  end
+
   def test_it_wins
-    mm = Mastermind.new
-    result = mm.execute("BBGB")
+    result = @mm.execute('rrgb')
     assert result.downcase.include?("win")
   end
+
+
+
+
 end
